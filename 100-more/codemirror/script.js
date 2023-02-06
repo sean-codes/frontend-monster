b0a75e3f875d12b19b1dee6a3da315c3b9fdd49e
Sean Codes <sean_codes@outlook.com>
2023-02-05 22:03:39 -0600

    save

--- 100-more/codemirror/script.js
+++ 100-more/codemirror/script.js
@@ -12,18 +12,26 @@ console.log(editors)
 var grabbed = undefined
 var grabbedPos = { x: 0, y: 0 }
 var grabbers = [...document.querySelectorAll('.grabber')].map((html, i) => {
-   var size = html.getBoundingClientRect()
+   var grabberSize = html.getBoundingClientRect()
    var editor = editors[i]
    var editorSize = editor.getBoundingClientRect()
-   console.log(editorSize)
    html.onmousedown = onEditorMouseDown
+   // console.log(editorSize)
 
+   var width = editorSize.width - grabberSize.width
+   // console.log(width)
+   var widthPercent = ((htmlEditors.getBoundingClientRect().width / 3) - grabberSize.width) / htmlEditors.getBoundingClientRect().width
+   editor.style.width = 100* widthPercent + '%'
+   // console.log(widthPercent)
    return {
       html: html,
       editor: editors[i],
       width: editorSize.width,
-      widthPercent: 1/3,
-      x: size.x
+      widthPercent: widthPercent,
+      x: grabberSize.x,
+      size: function() {
+         return this.editor.getBoundingClientRect()
+      }
    }
 })
 
@@ -36,44 +44,79 @@ function onEditorsMouseUp(e) {
    grabbed = undefined
 }
 function onEditorsMouseMove(e) {
-   // console.log('moved', e.clientX, e.clientY)
    e.preventDefault()
 
    if (grabbed) {
       var index = grabbers.indexOf(grabbed)
-      if (index <= 0) return
+      if (index <= 0) return // can not move first in group
 
-      var grabbedBefore = grabbers[index - 1]
-      var grabbedAfter = grabbers[index + 1]
-      var totalWidth = htmlEditors.getBoundingClientRect().width//grabbedBefore.width + grabbed.width
-      var totalWidthPercent = grabbedBefore.widthPercent + grabbed.widthPercent
+
+      var totalWidth = htmlEditors.getBoundingClientRect().width
+      var grabberWidth = grabbers[0].html.getBoundingClientRect().width
 
       var change = e.clientX - grabbedPos.x
-      var changePercent = change / totalWidth
-      console.log(changePercent)
-      // console.log(totalWidthPercent)
 
-      grabbedBefore.widthPercent += changePercent
-      grabbed.widthPercent -= changePercent
+      // console.log(changePercent)
 
-      if (grabbed.widthPercent < 0) {
-         if (change > 0 && grabbedAfter) {
-            console.log('move over')
-            grabbedAfter.widthPercent += grabbed.widthPercent
-            grabbed.widthPercent = 0
+      // there is some recurse way to do this :<
+
+      // going right
+      if (change > 0) {
+         var before = grabbers[index - 1]
+         var next = grabbers[index + 1]
+
+         if (next) {
+            var spaceLeft = Math.max(next.size().width, grabbed.size().width)
+            change = Math.min(Math.abs(change), spaceLeft) * Math.sign(change)
          }
 
-         if (change < 0 && grabbedBefore) {
-            
+         var changePercent = change / totalWidth
+
+         grabbed.widthPercent -= changePercent
+         before.widthPercent += changePercent
+
+         if (next && grabbed.widthPercent < 0) {
+            next.widthPercent += grabbed.widthPercent
+            grabbed.widthPercent = 0
+
+            if (next.widthPercent < 0) {
+               before.widthPercent -= next.widthPercent
+               next.widthPercent = 0
+            }
          }
       }
 
-      grabbedBefore.editor.style.width = `calc(${100* grabbedBefore.widthPercent + '%'} - 1rem)`
-      grabbed.editor.style.width = `calc(${100* grabbed.widthPercent + '%'} - 1rem)`
-      grabbedAfter.editor.style.width = `calc(${100* grabbedAfter.widthPercent + '%'} - 1rem)`
-      // console.log('resize', index)
-      // console.log(totalWidth)
+      // going left
+      if (change < 0) {
+         var before = grabbers[index - 1]
+         var beforeBefore = grabbers[index - 2]
+         var next = grabbers[index + 1]
+         var befores = grabbers.slice(0, index)
 
+
+         if (before) {
+            var spaceLeft = Math.max(...befores.map(b => b.size().width))
+            change = Math.min(Math.abs(change), spaceLeft) * Math.sign(change)
+         }
+
+         var changePercent = change / totalWidth
+
+         grabbed.widthPercent -= changePercent
+         before.widthPercent += changePercent
+
+         if (before && before.widthPercent < 0) {
+            beforeBefore.widthPercent += before.widthPercent
+            before.widthPercent = 0
+         }
+      }  
+
+      // update all widths
+      for (var grabber of grabbers) {
+         grabber.editor.style.width = grabber.widthPercent*100 + '%'
+      }
+
+
+      // update last grab point
       grabbedPos.x = e.clientX
    }
 }
