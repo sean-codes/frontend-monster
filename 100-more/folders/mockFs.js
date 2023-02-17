07e5e327e138d343d473159cac1be1fa34f51c6c
Sean Codes <sean_codes@outlook.com>
2023-02-16 21:02:38 -0600

    save folder

--- 100-more/folders/mockFs.js
+++ 100-more/folders/mockFs.js
@@ -1,36 +1,188 @@
-var mockFiles = [
-   { name: 'folder a', type: 'folder', files: [
-      { name: 'item z' },
-      { name: 'item x' },
-      { name: 'folder a 1', type: 'folder', files: [
-         { name: 'item a' },
-         { name: 'item b' },
+//---------------------------------------------------------------
+// Mock File System
+//---------------------------------------------------------------
+var mockFiles = {
+   name: 'root',
+   files: [
+      { name: 'folder_a', type: 'folder', files: [
+         { name: 'item z' },
+         { name: 'item x' },
+         { name: 'folder a 1', type: 'folder', files: [
+            { name: 'item a' },
+            { name: 'item b' },
+         ]},
+         { name: 'folder_a_2', type: 'folder', files: [
+            { name: 'item_b' },
+            { name: 'item_a' },
+         ]},
       ]},
-      { name: 'folder a 2', type: 'folder', files: [
-         { name: 'item a' },
-         { name: 'item b' },
+      { name: 'folder_c', type: 'folder', files: [
+         { name: 'item z' },
+         { name: 'item x' },
+         { name: 'folder c 1', type: 'folder', files: [
+            { name: 'item a' },
+            { name: 'item b' },
+         ]},
+         { name: 'folder c 2', type: 'folder', files: [
+            { name: 'item a' },
+            { name: 'item b' },
+         ]},
       ]},
-   ]},
-   { name: 'folder c', type: 'folder', files: [
-      { name: 'item z' },
-      { name: 'item x' },
-      { name: 'folder c 1', type: 'folder', files: [
-         { name: 'item a' },
-         { name: 'item b' },
-      ]},
-      { name: 'folder c 2', type: 'folder', files: [
-         { name: 'item a' },
-         { name: 'item b' },
-      ]},
-   ]},
-]
+   ]
+}
 
-export function fsMoveFile(from, to) {
-   console.log('move: '+ from)
-   console.log('to: ' + to)
+//---------------------------------------------------------------
+// Functions
+//---------------------------------------------------------------
+export function readDirSync(path) {
+   var dir = osTraverseToPath(path)
+   if (dir.files) {
+      var sorted = [...dir.files]
+      sorted
+         .sort((a, b) => a.name < b.name ? 1 : -1)
+         .sort((a, b) => a.folder && !b.folder ? -1 : 1)
+
+      return sorted.map(f => f.name)
+   }
+}
+
+
+export function renameSync(oldPath, newPath) {
+   var oldInfo = osPathInfo(oldPath)
+   var newInfo = osPathInfo(newPath)
+   var oldFile = osTraverseToPath(oldPath)
+
+   var parentPath = oldFile.type === 'folder' ? oldPath : oldInfo.folder // wrong
+   if (oldFile.type === 'folder' && newInfo.folder.startsWith(parentPath)) {
+      console.error('fs.renameSync: can not more parent into child')
+      return false
+   }
+
+   var oldFolder = osTraverseToPath(oldInfo.folder)
+   var newFolder = osTraverseToPath(newInfo.folder)
    
+   var copy = osTraverseToPath(oldPath)
+
+   // remove from old
+   oldFolder.files = oldFolder.files.filter(f => {
+      return f.name !== oldInfo.name
+   })
+
+   newFolder.files.push(copy)
+   debug.innerHTML = JSON.stringify(mockFiles, null, 4)
+   return true
 }
 
-export function readDir() {
-   return mockFiles
+function osPathInfo(path) {
+   var pathSplit = path.split('/')
+   var name = pathSplit.pop()
+   var folder = pathSplit.join('/')
+
+   return { name, folder, path }
 }
+
+function osTraverseToPath(path) {
+   var parts = path.split('/')
+   var pointer = mockFiles
+   var root = mockFiles.files
+
+   try {
+      for (var part of parts) {
+         var partTrim = part.trim().replaceAll('/', '')
+         if (!partTrim) continue
+
+         pointer = root.find(f => {
+            return f.name == partTrim
+         })
+
+         root = pointer.files
+      }
+
+      return pointer
+   } catch(e) {
+      return undefined
+   }
+}
+
+
+
+//---------------------------------------------------------------
+// Tests
+//---------------------------------------------------------------
+export function tests() {
+   var testFunctions = [testReadDirSync, testOsTraverseToPath, testRenameSync]
+
+   for (var test of testFunctions) {
+      // setup
+      var saveError = console.error
+      console.error = () => {}
+      var saveMockFiles = mockFiles
+      mockFiles = JSON.parse(JSON.stringify(mockFiles))
+
+      test()
+
+      //reset
+      mockFiles = saveMockFiles
+      console.error = saveError
+   }
+}
+
+function testOsTraverseToPath() {
+   var testCases = [
+      { path: '/folder_a', expect: mockFiles.files[0]},
+      { path: '/folder_c', expect: mockFiles.files[1]},
+   ]
+
+   for (var test of testCases) {
+      var receive = JSON.stringify(osTraverseToPath(test.path))
+      var expect = JSON.stringify(test.expect)
+
+      console.assert(
+         receive === expect, 
+         `
+         Test fs.osTraverseToPath: ${test.path}
+         Received: ${receive} 
+         Expected: ${expect}
+         `
+      )
+   }
+}
+
+function testReadDirSync() {
+   var testCases = [
+      { path: '', expect: ['folder_a', 'folder_c'] },
+      { path: '/folder_a', expect: ['folder a 1', 'folder_a_2', 'item x', 'item z' ] },
+      { path: 'folder_a', expect: ['folder a 1', 'folder_a_2', 'item x', 'item z'] },
+      { path: 'folder_a/folder a 1', expect: ['item a', 'item b'] },
+      { path: '/folder_a/folder a 1', expect: ['item a', 'item b'] },
+      { path: '/folder_a/folder_a_2', expect: ['item_a', 'item_b'] },
+      { path: '/folder_a/item z', expect: undefined },
+   ]
+
+
+   for (var test of testCases) {
+      var receivedFiles = JSON.stringify(readDirSync(test.path))
+      var expectFiles = JSON.stringify(test.expect)
+
+      console.assert(
+         receivedFiles == expectFiles, 
+         `
+         Test fs.ReadDirSync: ${test.path} 
+         Received: ${receivedFiles} 
+         Expected: ${expectFiles}
+         `
+      )
+   }
+}
+
+function testRenameSync() {
+   var beforeRename = JSON.stringify(mockFiles)
+
+   var worked = renameSync('/folder_a', '/folder_a/folder a 1/folder_a')
+   console.assert(!worked, 'Test renameSync: can not move parent into child')
+   console.assert(
+      JSON.stringify(mockFiles) == beforeRename, 
+      'Test renameSync: can not move parent into child'
+   )
+}
+
--- 100-more/folders/script.js
+++ 100-more/folders/script.js
@@ -1,230 +1,371 @@
-var mockFiles = [
-   { name: 'folder_a', type: 'folder', files: [
-      { name: 'item_z' },
-      { name: 'item_x' },
-      { name: 'folder_a 1', type: 'folder', files: [
-         { name: 'item_a' },
-         { name: 'item_b' },
-      ]},
-      { name: 'folder_a 2', type: 'folder', files: [
-         { name: 'item_a' },
-         { name: 'item_b' },
-      ]},
-   ]},
-   { name: 'folder_c', type: 'folder', files: [
-      { name: 'item_z' },
-      { name: 'item_x' },
-      { name: 'atem_x' },
-      { name: 'folder_c_2', type: 'folder', files: [
-         { name: 'item_a' },
-         { name: 'item_b' },
-      ]},
-      { name: 'a_folder_c_1', type: 'folder', files: [
-         { name: 'item_a' },
-         { name: 'item_b' },
-      ]},
-      { name: 'folder_aaa', type: 'folder', files: [
-         { name: 'item_a' },
-         { name: 'item_b' },
-      ]},
-   ]},
-]
+import * as fs from "./mockFs.js"
 
-// this operation is not really this complex. could send these references in
-// the flat files or on the up/down events. was sort of curious what might
-// the inside of a bash mv command look like. which i still sort of cheated
-// and reused the curseFiles function. the fsGetFile would likely split on /
-// then navigate in one at a time.
-function fsMoveFile(from, to) {
-   // console.log('move: '+ from)
-   // console.log('to: ' + to)
+console.log(fs)
 
-   var fromFile = fsGetFile(from)
-   var fromDir = fromFile.dir
-   var toFile = fsGetFile(to)
-   var toDir = toFile.type === 'folder' ? toFile : toFile.dir
+fs.tests()
 
-   // console.log(fromFile, fromDir)
-   // console.log(toFile, toDir)
-   if (fromDir && toDir) {
-      // split out the file
-      var fromIndex = fromDir.files.findIndex(f => f.name === from.split('/').pop())
-      var moved = fromDir.files.splice(fromIndex, 1).pop()
+var root = curseReadDir('')
+render()
 
-
-      toDir.files.push(moved)
-   }
-
-   // sort
-   curseFsSort(mockFiles)
-   console.log(mockFiles)
-
-   renderFiles()
+function update() {
+   root = curseReadDir('')
+   render()
 }
 
-function curseFsSort(files) {
+function curseReadDir(path) {
+   var readDir = fs.readDirSync(path)
+
+   return !readDir
+      ? new File(path)
+      : new Folder(path, readDir)
+}
+
+function File(path) {
+   this.path = path
+   var pathSplit = path.split('/')
+   this.name = pathSplit.pop()
+   this.folder = pathSplit.join('/')
+   this.type = 'file'
+
+   return this
+}
+
+function Folder(path, files) {
+   this.type = 'folder' // lots of duplicates..
+   this.files = []
+   this.path = path
+   var pathSplit = path.split('/')
+   this.name = pathSplit.pop()
+   this.folder = pathSplit.join('/')
+
    for (var file of files) {
-      if (file.type == 'folder') {
-         curseFsSort(file.files)
-         console.log('sort', file.name)
-         file.files.sort((a, b) => {
-            var testString = a.name < b.name
-            return testString ? -1 : 1
-         })
-         file.files.sort((a, b) => {
-            return a.type === 'folder' && b.type !== 'folder' ? -1 : 1
-         })
-      }
+      this.files.push(curseReadDir(path + '/' + file))
+   }
+
+   return this
+}
+
+function render() {
+   var html = renderFolder(root)
+   var htmlFolders = document.querySelector('.folders')
+   htmlFolders.innerHTML = html
+}
+
+function renderFolder(folder, depth = -1) {
+   var html = ''
+   for (var file of folder.files) {
+      html += file.type === 'folder'
+         ? renderFolder(file, depth + 1)
+         : renderFile(file, depth + 1)
+   }
+
+   return `
+      ${renderFile(folder, depth)}
+      ${html}
+   `  
+}
+
+function renderFile(file, depth) {
+   return `
+      <div 
+         class="file" 
+         style="--depth: ${depth}"
+         data-folder="${file.folder}"
+         data-path="${file.path}"
+      >
+         ${file.name}
+      </div>
+   `
+}
+
+
+// Listeners / Drag and drop
+document.body.addEventListener('mousedown', e => {
+   if (e.target.classList.contains('folder')) {
+      // onMousedownFolder(e.target)
+      onMousedownFile(e.target)
+   }
+
+   if (e.target.classList.contains('file')) {
+      onMousedownFile(e.target)
+   }
+})
+document.body.addEventListener('mousemove', onMousemove)
+document.body.addEventListener('mouseup', onMouseup)
+
+
+
+var down = undefined
+var over = undefined
+
+
+function onMousemove(e) {
+   // console.log(e)
+   var eleOver = e.target
+   var overFile = eleOver.classList.contains('file')
+   var overFolder = eleOver.classList.contains('folder')
+
+   // remove old over
+   var oldOver = document.querySelector('.over')
+   oldOver && oldOver.classList.remove('over')
+   over = undefined
+   if (overFile || overFolder) {
+      var folder = overFile
+         ? document.querySelector(`[data-path="${eleOver.dataset.folder}"]`)
+         : eleOver
+
+      folder.classList.add('over')
+      over = overFile 
+         ? eleOver.dataset.folder 
+         : eleOver.dataset.path
+   }
+
+}
+
+function onMousedownFolder(ele) {
+   console.log('clicked folder', ele)
+}
+
+function onMouseup(ele) {
+   if (down && over) {      
+      var downName = down.split('/').pop()
+      fs.renameSync(down, over + '/' + downName)
+      update()
    }
 }
 
-
-function fsGetFile(path) {
-   var dir = undefined
-   var dirPath = path.slice(0, path.lastIndexOf('/'))
-
-   // it really would not work this way
-   curseFiles(mockFiles, f => {
-      var pathPlusName = f.path + f.name
-      if (pathPlusName === path) 
-         fromDir = f
-   })
-
-   return fromDir
+function onMousedownFile(ele) {
+   down = ele.dataset.path
+   console.log(down)
 }
 
+// var mockFiles = [
+//    { name: 'folder_a', type: 'folder', files: [
+//       { name: 'item_z' },
+//       { name: 'item_x' },
+//       { name: 'folder_a 1', type: 'folder', files: [
+//          { name: 'item_a' },
+//          { name: 'item_b' },
+//       ]},
+//       { name: 'folder_a 2', type: 'folder', files: [
+//          { name: 'item_a' },
+//          { name: 'item_b' },
+//       ]},
+//    ]},
+//    { name: 'folder_c', type: 'folder', files: [
+//       { name: 'item_z' },
+//       { name: 'item_x' },
+//       { name: 'atem_x' },
+//       { name: 'folder_c_2', type: 'folder', files: [
+//          { name: 'item_a' },
+//          { name: 'item_b' },
+//       ]},
+//       { name: 'a_folder_c_1', type: 'folder', files: [
+//          { name: 'item_a' },
+//          { name: 'item_b' },
+//       ]},
+//       { name: 'folder_aaa', type: 'folder', files: [
+//          { name: 'item_a' },
+//          { name: 'item_b' },
+//       ]},
+//    ]},
+// ]
 
-var htmlEvents = document.querySelector('.events')
-var htmlFolders = document.querySelector('.folders')
-var htmlHeld = document.querySelector('.held')
-document.body.addEventListener('mousemove', onMouseMove)
-document.body.addEventListener('mouseup', onMouseUp)
-document.body.addEventListener('mouseleave', onMouseUp)
+// // this operation is not really this complex. could send these references in
+// // the flat files or on the up/down events. was sort of curious what might
+// // the inside of a bash mv command look like. which i still sort of cheated
+// // and reused the curseFiles function. the fsGetFile would likely split on /
+// // then navigate in one at a time.
+// function fsMoveFile(from, to) {
+//    // console.log('move: '+ from)
+//    // console.log('to: ' + to)
 
+//    var fromFile = fsGetFile(from)
+//    var fromDir = fromFile.dir
+//    var toFile = fsGetFile(to)
+//    var toDir = toFile.type === 'folder' ? toFile : toFile.dir
 
-var state = {
-   held: undefined,
-   heldX: 0,
-   heldY: 0,
-   events: []
-}
-
-curseFsSort(mockFiles)
-renderFiles(mockFiles)
-
-
-function onMouseMove(e) {
-   htmlHeld.style.display = state.held ? 'block' : 'none'
-   htmlFolders.classList.toggle('grabbing', state.held ? true : false)
-
-   if (state.held) {
-      htmlHeld.innerHTML = state.held.name
-      htmlHeld.style.top = e.clientY + 'px'
-      htmlHeld.style.left = e.clientX + 'px'
-   }
-}
-
-function onMouseUp(e) {
-   htmlHeld.style.display = 'none'
-   state.held = undefined
-}
-
-
-function onFileMouseUp(e) {
-   var fileUp = flatFiles(mockFiles)[this.dataset.flatId]
-   var fileDown = state.held
-
-   var fileFrom = fileDown.path + fileDown.name
-   var fileTo = fileUp.path + fileUp.name
-
-   if (fileFrom !== fileTo && !fileTo.startsWith(fileFrom)) {
-      fsMoveFile(fileFrom, fileTo)
-   }
-
-   state.held = undefined
-}
-
-
-function onFileMouseOver(e) {
-   if (state.held) {
-      var flattenedFiles = flatFiles(mockFiles)
-      var file = flattenedFiles[this.dataset.flatId]
-      // var folder = file.dir
-      // console.log(file.dir)
-      var i = this.dataset.flatId // ok
-      if (file.type !== 'folder') {
-         for (i = this.dataset.flatId; i > 0; i--) {
-            var folder = flattenedFiles[i]
-            var isFolder = folder.type === 'folder'
-            var hasFile = isFolder && folder.files.some(f => file.path + f.name === file.full)
-            if (isFolder && hasFile) {
-               file = folder
-               break
-            }
-         }
-      }
-
-      // remove / add
-      var htmlCurrentlyOver = document.querySelector('.over')
-      htmlCurrentlyOver && htmlCurrentlyOver.classList.remove('over')
-
-      var newOver = document.querySelector(`[data-flat-id="${i}"`)
-      newOver.classList.add('over')
-   }
-}
-
-function onFileMouseDown(e) {
-   var file = flatFiles(mockFiles)[this.dataset.flatId]
-   state.held = file
-   htmlFolders.classList.toggle('grabbing', state.held ? true : false)
-}
+//    // console.log(fromFile, fromDir)
+//    // console.log(toFile, toDir)
+//    if (fromDir && toDir) {
+//       // split out the file
+//       var fromIndex = fromDir.files.findIndex(f => f.name === from.split('/').pop())
+//       var moved = fromDir.files.splice(fromIndex, 1).pop()
 
 
-function renderFiles(files) {
-   htmlFolders.innerHTML = ''
+//       toDir.files.push(moved)
+//    }
 
-   var flattened = flatFiles(mockFiles)
-   for (var i in flattened) {
-      var flat = flattened[i]
-      htmlFile = document.createElement('button')
-      htmlFile.classList.add('file')
-      htmlFile.setAttribute('style', `--depth: ${flat.depth}`)
-      htmlFile.innerText = flat.name
-      htmlFile.tabIndex = 1
-      htmlFile.dataset.flatId = i
-      htmlFolders.appendChild(htmlFile)
-      htmlFile.addEventListener('mousedown', onFileMouseDown)
-      htmlFile.addEventListener('mouseup', onFileMouseUp)
-      htmlFile.addEventListener('mouseover', onFileMouseOver)
-   }
-}
+//    // sort
+//    curseFsSort(mockFiles)
+//    console.log(mockFiles)
 
+//    renderFiles()
+// }
 
-function flatFiles(files) {
-   var flat = []
-   curseFiles(files, (file) => flat.push(file))
-   return flat
-}
-
-
-function curseFiles(files, callback, dir, path = '', depth = 0) {
-   for (var i = 0; i < files.length; i++) {
-      var file = files[i]
-      var filePath = path
-      callback({ // ./
-         ...file, // ./info
-         depth: depth, // for display
-         full: filePath + file.name,
-         path: filePath, // ..
-         dir: dir || { files }, // ..info
-      })
+// function curseFsSort(files) {
+//    for (var file of files) {
+//       if (file.type == 'folder') {
+//          curseFsSort(file.files)
+//          console.log('sort', file.name)
+//          file.files.sort((a, b) => {
+//             var testString = a.name < b.name
+//             return testString ? -1 : 1
+//          })
+//          file.files.sort((a, b) => {
+//             return a.type === 'folder' && b.type !== 'folder' ? -1 : 1
+//          })
+//       }
+//    }
+// }
 
 
-      if (file.type === 'folder') {
-         folderPath = path + file.name + '/'
-         curseFiles(file.files, callback, file, folderPath, depth + 1)
-      }
-   }
-}
+// function fsGetFile(path) {
+//    var dir = undefined
+//    var dirPath = path.slice(0, path.lastIndexOf('/'))
+
+//    // it really would not work this way
+//    curseFiles(mockFiles, f => {
+//       var pathPlusName = f.path + f.name
+//       if (pathPlusName === path) 
+//          fromDir = f
+//    })
+
+//    return fromDir
+// }
+
+
+// var htmlEvents = document.querySelector('.events')
+// var htmlFolders = document.querySelector('.folders')
+// var htmlHeld = document.querySelector('.held')
+// document.body.addEventListener('mousemove', onMouseMove)
+// document.body.addEventListener('mouseup', onMouseUp)
+// document.body.addEventListener('mouseleave', onMouseUp)
+
+
+// var state = {
+//    held: undefined,
+//    heldX: 0,
+//    heldY: 0,
+//    events: []
+// }
+
+// curseFsSort(mockFiles)
+// renderFiles(mockFiles)
+
+
+// function onMouseMove(e) {
+//    htmlHeld.style.display = state.held ? 'block' : 'none'
+//    htmlFolders.classList.toggle('grabbing', state.held ? true : false)
+
+//    if (state.held) {
+//       htmlHeld.innerHTML = state.held.name
+//       htmlHeld.style.top = e.clientY + 'px'
+//       htmlHeld.style.left = e.clientX + 'px'
+//    }
+// }
+
+// function onMouseUp(e) {
+//    htmlHeld.style.display = 'none'
+//    state.held = undefined
+// }
+
+
+// function onFileMouseUp(e) {
+//    var fileUp = flatFiles(mockFiles)[this.dataset.flatId]
+//    var fileDown = state.held
+
+//    var fileFrom = fileDown.path + fileDown.name
+//    var fileTo = fileUp.path + fileUp.name
+
+//    if (fileFrom !== fileTo && !fileTo.startsWith(fileFrom)) {
+//       fsMoveFile(fileFrom, fileTo)
+//    }
+
+//    state.held = undefined
+// }
+
+
+// function onFileMouseOver(e) {
+//    if (state.held) {
+//       var flattenedFiles = flatFiles(mockFiles)
+//       var file = flattenedFiles[this.dataset.flatId]
+//       // var folder = file.dir
+//       // console.log(file.dir)
+//       var i = this.dataset.flatId // ok
+//       if (file.type !== 'folder') {
+//          for (i = this.dataset.flatId; i > 0; i--) {
+//             var folder = flattenedFiles[i]
+//             var isFolder = folder.type === 'folder'
+//             var hasFile = isFolder && folder.files.some(f => file.path + f.name === file.full)
+//             if (isFolder && hasFile) {
+//                file = folder
+//                break
+//             }
+//          }
+//       }
+
+//       // remove / add
+//       var htmlCurrentlyOver = document.querySelector('.over')
+//       htmlCurrentlyOver && htmlCurrentlyOver.classList.remove('over')
+
+//       var newOver = document.querySelector(`[data-flat-id="${i}"`)
+//       newOver.classList.add('over')
+//    }
+// }
+
+// function onFileMouseDown(e) {
+//    var file = flatFiles(mockFiles)[this.dataset.flatId]
+//    state.held = file
+//    htmlFolders.classList.toggle('grabbing', state.held ? true : false)
+// }
+
+
+// function renderFiles(files) {
+//    htmlFolders.innerHTML = ''
+
+//    var flattened = flatFiles(mockFiles)
+//    for (var i in flattened) {
+//       var flat = flattened[i]
+//       htmlFile = document.createElement('button')
+//       htmlFile.classList.add('file')
+//       htmlFile.setAttribute('style', `--depth: ${flat.depth}`)
+//       htmlFile.innerText = flat.name
+//       htmlFile.tabIndex = 1
+//       htmlFile.dataset.flatId = i
+//       htmlFolders.appendChild(htmlFile)
+//       htmlFile.addEventListener('mousedown', onFileMouseDown)
+//       htmlFile.addEventListener('mouseup', onFileMouseUp)
+//       htmlFile.addEventListener('mouseover', onFileMouseOver)
+//    }
+// }
+
+
+// function flatFiles(files) {
+//    var flat = []
+//    curseFiles(files, (file) => flat.push(file))
+//    return flat
+// }
+
+
+// function curseFiles(files, callback, dir, path = '', depth = 0) {
+//    for (var i = 0; i < files.length; i++) {
+//       var file = files[i]
+//       var filePath = path
+//       callback({ // ./
+//          ...file, // ./info
+//          depth: depth, // for display
+//          full: filePath + file.name,
+//          path: filePath, // ..
+//          dir: dir || { files }, // ..info
+//       })
+
+
+//       if (file.type === 'folder') {
+//          folderPath = path + file.name + '/'
+//          curseFiles(file.files, callback, file, folderPath, depth + 1)
+//       }
+//    }
+// }
 
 
