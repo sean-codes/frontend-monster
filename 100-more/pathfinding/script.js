7b5c95a66136f974c898e1234120fb62f461601f
Sean Codes <sean_codes@outlook.com>
2024-12-05 03:02:36 -0600

    save editing

--- 100-more/pathfinding/script.js
+++ 100-more/pathfinding/script.js
@@ -6,7 +6,8 @@ import { LineMaterial } from 'three/addons/lines/LineMaterial.js';
 
 // config
 var cw = 15
-var space = cw/10
+var spaces = 10
+var space = cw/spaces
 var offset = cw/2 - space/2
 
 // setup
@@ -19,10 +20,13 @@ document.body.appendChild(renderer.domElement)
 
 // camera
 var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = -5
-camera.position.x = -15
+camera.position.z = -8
+camera.position.x = -8
+
+// camera.position.z += offset
+// camera.position.x += offse20
 camera.position.y = 20
-camera.lookAt(0, 0, 0)
+camera.lookAt(offset, 0, offset)
 
 
 // light
@@ -35,7 +39,7 @@ pointLight.castShadow = true
 scene.add(pointLight)
 
 
-// shapes
+// floor
 var geometry = new THREE.BoxGeometry(cw, 1, cw)
 var material = new THREE.MeshPhongMaterial({ color: '#333335' })
 material.shininess = 0
@@ -45,7 +49,28 @@ floor.position.x += offset
 floor.position.z += offset
 floor.position.y -= space/2 + 0.5
 floor.receiveShadow = true
+floor.isFloor = true
 scene.add(floor)
+
+// floor cursor
+var cursorObj = new THREE.Object3D()
+var points = [
+   0, -space/2+0.1, 0,
+   space, -space/2+0.1, 0,
+   space, -space/2+0.1, space,
+   0, -space/2+0.1, space,
+   0, -space/2+0.1, 0,
+]
+var geo = new LineGeometry();
+geo.setPositions( points );
+var mat = new LineMaterial({ color: '#CCC', linewidth: 0.1, worldUnits: true })
+var line = new Line2(geo, mat)
+line.computeLineDistances()
+cursorObj.add(line)
+line.position.x -= space/2
+line.position.z -= space/2
+scene.add(cursorObj)
+
 
 
 // controls
@@ -54,7 +79,36 @@ controls.listenToKeyEvents( window );
 controls.maxPolarAngle = Math.PI / 2;
 controls.target = floor.position
 controls.update()
+controls.rotateSpeed = 0.5
 
+// mouse
+var raycaster = new THREE.Raycaster()
+var pointer = new THREE.Vector2(-1, -1)
+var holding = false
+var hoverX = -1
+var hoverY = -1
+window.addEventListener('pointermove', function() {
+   pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+   pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+})
+
+window.addEventListener('mousedown', function() {
+   if (hoverX >= 0 && hoverY >=0) {
+      console.log('click area', hoverX, hoverY)
+      var current = map[hoverX][hoverY]
+
+      if (current == 'x' || current == '-') {
+         map[hoverX][hoverY] = current == 'x' ? '-' : 'x'
+      }
+      if (current == 's' || current == 'e') {
+         holding = holding ? false : {
+            old: { x: hoverX, y: hoverY },
+            type: current
+         }
+      }
+      loadMap()
+   }
+})
 // lines
 // var points = [
 //    -8, 3, -8,
@@ -63,24 +117,55 @@ controls.update()
 // ]
 // var geo = new LineGeometry();
 // geo.setPositions( points );
-// var mat = new LineMaterial({ color: '#FF6961', linewidth: 0.1, worldUnits: true })
+// var mat = new LineMaterial({ color: '#FF6961', linewidth: 0.1, worldUnits: true, dashed: true })
 // var line = new Line2(geo, mat)
+// line.computeLineDistances()
 // scene.add(line)
 
 
 
-function step() {
-   renderer.render(scene, camera)
 
-   // cube.rotation.x += 0.01
-   // cube.rotation.y += 0.01
+// step
+function step() {
+   raycaster.setFromCamera(pointer, camera)
+   var intersects = raycaster.intersectObjects(scene.children)
+   var top = intersects[0]
+   var floor = intersects.find(o => o.object.isFloor)
+   hoverX = -1
+   hoverY = -1
+   scene.remove(cursorObj)
+   if (floor && floor.normal.y == 1) {
+      scene.add(cursorObj)
+
+      var xLock = Math.floor(floor.uv.x*spaces)
+      var yLock = spaces - Math.ceil(floor.uv.y*spaces)
+
+      cursorObj.position.x = xLock * space
+      cursorObj.position.z = yLock * space
+      
+      hoverX = xLock
+      hoverY = yLock
+
+      if (holding) {
+         var over = map[hoverX][hoverY]
+         if (over !== 'x') {
+            map[holding.old.x][holding.old.y] = '-'
+            map[hoverX][hoverY] = holding.type
+            holding.old.x = hoverX
+            holding.old.y = hoverY
+            loadMap()
+         }
+      }
+   }
+
+
    for (var object of objects) {
       object.draw()
    }
-   // renderer.setAnimationLoop(null)
-}
 
-// step()
+   renderer.render(scene, camera)
+
+}
 
 renderer.setAnimationLoop(step)
 
@@ -89,68 +174,92 @@ renderer.setAnimationLoop(step)
 var objects = []
 var map = `
 ----------
-----------
+----x-----
 ----x-----
 -s--x-----
 ----x-----
 ----x--e--
 ----x-----
-----------
-----------
+----x-----
+---xx-----
 ----------
 `.trim().split('\n').map(r => r.split(''))
 
 var startObject, endObject
 
-// load objects
-for (var rowI in map) {
-   for (var colI in map[rowI]) {
-      var object = {
-         's': ObjectStart,
-         'x': ObjectBlock,
-         'e': ObjectEnd,
-      }[map[rowI][colI]]
+function loadMap() {
+   for (var object of objects) {
+      scene.remove(object.shape)
+   }
+   objects = []
+   // load objects
+   for (var rowI in map) {
+      for (var colI in map[rowI]) {
+         var object = {
+            's': ObjectStart,
+            'x': ObjectBlock,
+            'e': ObjectEnd,
+         }[map[rowI][colI]]
 
-      object && objects.push(new object(colI*space, rowI*space))
+         object && objects.push(new object(colI*space, rowI*space))
+      }
    }
 }
-
+loadMap()
 var sceneLine
+var sceneLineOffset = 0
 
 function ObjectStart(x, y) {
    startObject = this
-   this.shape = createObjectShape(x, y, '#9455f4')
+   this.type = 's'
+   this.shape = createObjectShape(this, x, y, '#9455f4')
+
    this.x = x
    this.y = y
 
    this.draw = function() {
-      // console.log('test')
       // draw path to end
+      if (sceneLine) scene.remove(sceneLine)
       var path = findPath(
          { x: this.x, y: this.y},
          { x: endObject.x, y:endObject.y}
       )
       
+      if (!path) {
+         return
+      }
       var points = []
       var from = path
+      var last
       do {
+         last = from
          points.push(from.y, 0, from.x)
          from = from.from 
       } while(from)
-
+      sceneLineOffset += 0.025
+      
+      // very suboptimal 
       var geo = new LineGeometry();
       geo.setPositions( points );
-      if (!sceneLine) {
-         var mat = new LineMaterial({ color: '#FF6961', linewidth: 0.1, worldUnits: true })
-         sceneLine = new Line2(geo, mat)
-         scene.add(sceneLine)
-      }
+      var mat = new LineMaterial({ 
+         color: '#FF6961', 
+         linewidth: 0.1, 
+         worldUnits: true, 
+         gapSize:0.25, 
+         dashOffset: sceneLineOffset, 
+         dashSize: 0.5, 
+         dashed: true 
+      })
+      sceneLine = new Line2(geo, mat)
+      sceneLine.computeLineDistances()
+      scene.add(sceneLine)
    }
 }
 
 function ObjectEnd(x, y) {
    endObject = this
-   this.shape = createObjectShape(x, y, '#50C878')
+   this.type = 'e'
+   this.shape = createObjectShape(this, x, y, '#50C878')
    this.x = x
    this.y = y
 
@@ -160,7 +269,8 @@ function ObjectEnd(x, y) {
 }
 
 function ObjectBlock(x, y) {
-   this.shape = createObjectShape(x, y, '#DDDDD5')
+   this.type = 'x'
+   this.shape = createObjectShape(this, x, y, '#DDDDD5')
    this.x = x
    this.y = y
 
@@ -169,7 +279,7 @@ function ObjectBlock(x, y) {
    }
 }
 
-function createObjectShape(x, y, color) {
+function createObjectShape(object, x, y, color) {
    var geometry = new THREE.BoxGeometry(space, space, space)
    var material = new THREE.MeshPhongMaterial({ color })
    material.flatShading = true
@@ -179,7 +289,7 @@ function createObjectShape(x, y, color) {
    cube.position.z = x// - offset
    cube.position.x = y// - offset
    scene.add(cube)
-
+   cube.gameObject = object
    return cube
 }
 
@@ -255,6 +365,7 @@ function findPath(start, end) {
 
    check(checks[0])
 
+   // console.log(checks)
    var last = checks[checks.length-1].find(c => c.type == 'e')
    return last
 }
